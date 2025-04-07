0e5d9e25f60991557dfab79802943b54765cb8ce
Ivan Arefkov <ivanarefkov@gmail.com>
2025-04-07 10:57:57 -0700

    v0.1.2 security improvements

--- english_testing_app/settings.py
+++ english_testing_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@k9vk$#67-j@2m==)6=3x!8lfi#zmxgf-p09vbb4g*m8y&n&%!'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
