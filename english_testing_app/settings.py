a922562ae837f408e54280411742074cf8f2d9da
Ivan Arefkov <ivanarefkov@gmail.com>
2025-04-18 10:34:34 -0700

    v0.1.5

--- english_testing_app/settings.py
+++ english_testing_app/settings.py
@@ -17,7 +17,6 @@ dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Environment Variables
 MY_IP = str(os.getenv("MY_IP"))
 DEBUG_STATUS = bool(os.getenv("DEBUG"))
